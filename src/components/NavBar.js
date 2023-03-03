a77f491b55cd42913d8fd7c5ad9b7eedbf400fb2
Caren-Koroeny <carensiya5@gmail.com>
2023-03-03 11:18:40 +0300

    add navigation menu details

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,2 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
+const  NavigationMenu  = () => (
+    <section className="header-container">
+        <div className="header-section">
+        <h1>Bookstore CMS</h1>
+        <ul className="nav-links">
+        <li className="link"><Link to="/">BOOKS</Link></li>
+
+        <li className="link"><Link to="/category" style={{ opacity: '0.5' }}>CATEGORIES</Link></li>
+      </ul>
+        </div>
+    </section>
+);
+
+export default NavigationMenu;
+
