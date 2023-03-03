5f0ac8021a86842c51e4d21259dc094488672608
Caren-Koroeny <carensiya5@gmail.com>
2023-03-03 11:26:20 +0300

    add App details for routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,17 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import BookList from './components/BookList';
+import NavigationMenu from './components/NavBar';
+import Categories from './components/Categories';
 
-import BookL
+const App = () => (
+    <section className='mainContainer'>
+    <NavigationMenu />
+    <Routes>
+    <Route path='/' exact element={<BookList />} />
+    <Route path='/category' exact element={<Categories />} />
+    </Routes>
+    </section>
+);
+
+export default App
