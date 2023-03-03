554b46fbf2fe2838c2d4f032c65b190276b16c59
Caren-Koroeny <carensiya5@gmail.com>
2023-03-03 11:09:47 +0300

    add categories details

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,12 +1,12 @@
-// import React from "react";
-// import styles from '../styles/Categories.module.css';
+import React from "react";
+import styles from '../styles/Categories.module.css';
 
-// const Categories = () => (
+const Categories = () => (
 
-//     <>
-//     <div style={{ margin: 'auto'}}>   
-//     <button type="button" className={styles.categoriesUpdate}>Check Status</button>
-//     </div>
-//     </>
-// )
-// export default Categories;
+    <>
+    <div style={{ margin: 'auto'}}>   
+    <button type="button" className={styles.categoriesUpdate}>Check Status</button>
+    </div>
+    </>
+)
+export default Categories;
