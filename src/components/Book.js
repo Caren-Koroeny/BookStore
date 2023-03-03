40c67f4daf32e1b30a5ddafc183d78bd3da81505
Caren-Koroeny <carensiya5@gmail.com>
2023-03-03 13:12:47 +0300

    add styling on card-2

--- src/components/Book.js
+++ src/components/Book.js
@@ -22,7 +22,9 @@ const Book = (obj) => {
         <p className="percentage">64%</p>
         <p className="complete">Completed</p>
       </div>
+      <div className="hr" />
       <div className="card-3">
+        <p style={{ opacity: 0.5, fontSize: '13px' }}>CURRENT CHAPTER</p>
         <p style={{ textTransform: 'uppercase' }} className="chapter">chapter 17</p>
         <button type="button" style={{ textTransform: 'uppercase' }} className={styles.bookupdate}>Update Progress   </button>
       </div>
