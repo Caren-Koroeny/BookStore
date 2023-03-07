07a61113e6bd9167eac3723361b488a90a23bc6b
Caren-Koroeny <carensiya5@gmail.com>
2023-03-07 07:55:26 +0300

    used creatSlice in bookSlice.js

--- src/redux/books/bookSlice.js
+++ src/redux/books/bookSlice.js
@@ -1,37 +1,16 @@
-// action types
-const ADD_BOOK = 'ADD_BOOK';
-const REMOVE_BOOK = 'REMOVE_BOOK';
+import { createSlice } from '@reduxjs/toolkit';
 
-// an empty array as the initial state
-const initialState = {
-  booksArray: [],
-};
-// Reducer
-const BookReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case ADD_BOOK:
-      return {
-        ...state,
-        books: [...state, action.book],
-      };
-    case REMOVE_BOOK:
-      return {
-        ...state,
-        books: state.books.filter((book) => book.id !== action.bookId),
-      };
-    default:
-      return state;
-  }
-};
-// Action creators
-const AddBook = (book) => ({
-  type: ADD_BOOK,
-  book,
+const bookSlice = createSlice({
+  name: 'book',
+  initialState:{booksArray: [] },
+  reducers: {
+    addBook: (state, action ) => {
+      state.booksArray.pish(action.payload);
+    },
+    removeBook: (state,action) => {
+      state.booksArray = state.booksArray.filter((book) => book.id !==action.payload);
+    },
+  },
 });
 
-const RemoveBook = (bookId) => ({
-  type: REMOVE_BOOK,
-  bookId,
-});
-
-export { BookReducer, AddBook, RemoveBook };
+export const { addBook, removeBook} = bookSlice.actions;
