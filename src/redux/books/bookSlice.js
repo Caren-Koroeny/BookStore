e70c175466a20c09e56107e3b966d4db6b5e0dbc
Caren-Koroeny <carensiya5@gmail.com>
2023-03-09 13:23:23 +0300

    added post to API functionality

--- src/redux/books/bookSlice.js
+++ src/redux/books/bookSlice.js
@@ -1,52 +1,81 @@
 /* eslint-disable no-param-reassign */
-import { createSlice, nanoid } from '@reduxjs/toolkit';
+import { createSlice, nanoid, createAsyncThunk } from '@reduxjs/toolkit';
 
-const initialState = {
-  booksArray:
-  // Initial state:
-  [
-    {
-      item_id: '1',
-      title: 'The Great Gatsby',
-      author: 'John Smith',
-      category: 'Fiction',
-    },
-    {
-      item_id: '2',
-      title: 'Anna Karenina',
-      author: 'Leo Tolstoy',
-      category: 'Fiction',
-    },
-    {
-      item_id: '3',
-      title: 'The Selfish Gene',
-      author: 'Richard Dawkins',
-      category: 'Nonfiction',
-    },
-  ],
-};
-const bookSlice = createSlice({
-  name: 'books',
-  initialState,
-  reducers: {
-    AddBooks: (state, action) => {
-      const newBook = {
-        item_id: nanoid(),
-        title: action.payload.title,
-        author: action.payload.author,
-      };
-      return {
-        ...state,
-        booksArray: [...state.booksArray, newBook],
-      };
-    },
-    RemoveBook: (state, { payload }) => {
-      state.booksArray = state.booksArray.filter((books) => books.item_id !== payload);
-    },
 
-  },
-});
+const URL = 
+  "https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/m6KbdydZiVrXkOzJH2Gv";
+  
+  // get books from API
+  export const getBooksData = createAsyncThunk("books/getBooksData", async () => {
+    const response =  await axios(`${URL}/books`);
+    return response.data;
 
-export const { AddBooks, RemoveBook } = bookSlice.actions;
+  });
+
+  // POST TO API
+  export const AddBooks = createAsyncThunk(
+    "books/AddBooks",
+    async (payload, thunkApi) => {
+      try {
+        const response = await axios.post(`${URL}/books`, {
+          ...payload,
+          item_id: nanoid(),
+        });
+  
+        return response.data;
+      } catch (error) {
+        return thunkApi.rejectWithValue(
+          error.data.message || "something went wrong"
+        );
+      }
+    }
+  );
+// const initialState = {
+//   booksArray:
+//   // Initial state:
+//   [
+//     {
+//       item_id: '1',
+//       title: 'The Great Gatsby',
+//       author: 'John Smith',
+//       category: 'Fiction',
+//     },
+//     {
+//       item_id: '2',
+//       title: 'Anna Karenina',
+//       author: 'Leo Tolstoy',
+//       category: 'Fiction',
+//     },
+//     {
+//       item_id: '3',
+//       title: 'The Selfish Gene',
+//       author: 'Richard Dawkins',
+//       category: 'Nonfiction',
+//     },
+//   ],
+// };
+// const bookSlice = createSlice({
+//   name: 'books',
+//   initialState,
+//   reducers: {
+//     AddBooks: (state, action) => {
+//       const newBook = {
+//         item_id: nanoid(),
+//         title: action.payload.title,
+//         author: action.payload.author,
+//       };
+//       return {
+//         ...state,
+//         booksArray: [...state.booksArray, newBook],
+//       };
+//     },
+//     RemoveBook: (state, { payload }) => {
+//       state.booksArray = state.booksArray.filter((books) => books.item_id !== payload);
+//     },
+
+//   },
+// });
+
+// export const { AddBooks, RemoveBook } = bookSlice.actions;
 
 export default bookSlice.reducer;
