5e4768acdb345230b95a5bafc4ea2af5a6602e53
Caren-Koroeny <carensiya5@gmail.com>
2023-03-09 05:01:48 +0300

    post book to API function

--- src/redux/books/BookThunk.js
+++ src/redux/books/BookThunk.js
@@ -1,4 +1,5 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
+import { async } from "q";
 
 const APIID = 'XiaYGiqqbyc8T2fksKKK';
 const URL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApiapps/${APIKEY}/books/';
@@ -10,4 +11,18 @@ export const getBookData = createAsyncThunk('book/getBookData', async() => {
     }catch(error){
         return error;
     }
-})
+});
+
+export const AddBook = createAsyncThunk('book/AddBook', async(data, thumkAPI) => {
+    try {
+        return fetch(URL, {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-type':'application/json; charset=UTF-8',
+            },
+        }).then(() => data);
+    }catch (error){
+        return thumkAPI.rejectWithValue('something went wrong');
+    }
+});
